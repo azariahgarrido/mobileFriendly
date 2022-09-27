e5d8b48723f894c5cc4f5decb56d3f801dd035ca
azariahgarrido <azariahdanielgarrido@gmail.com>
2022-09-27 12:59:18 -0700

    Added navigation styling

--- client/src/components/Navigation.js
+++ client/src/components/Navigation.js
@@ -1,38 +1,43 @@
 export default function Navigation({ currentPage, handlePageChange }) {
     return (
         <div className="nav">
-            <button>
+
+            <button className="navButton">
                 <a
                     href="#AboutMe"
-                    // onClick={() => handlePageChange('AboutMe')}
-                    // className={currentPage === 'AboutMe' ? 'nav-linkActive' : 'nav-link'}
+                    onClick={() => handlePageChange('AboutMe')}
+                    className={currentPage === 'AboutMe' ? 'navTrue' : 'navFalse'}
                     >About Me
                 </a>
             </button>
-            <button>
+
+            <button className="navButton">
                 <a
                     href="#Portfolio"
-                    // onClick={() => handlePageChange('Portfolio')}
-                    // className={currentPage === 'Portfolio' ? 'nav-linkActive' : 'nav-link'}
+                    onClick={() => handlePageChange('Portfolio')}
+                    className={currentPage === 'Portfolio' ? 'navTrue' : 'navFalse'}
                     >Portfolio
                 </a>
             </button>
-            <button>
+
+            <button className="navButton">
                 <a
                     href="#Contact"
-                    // onClick={() => handlePageChange('Contact')}
-                    // className={currentPage === 'Contact' ? 'nav-linkActive' : 'nav-link'}
+                    onClick={() => handlePageChange('Contact')}
+                    className={currentPage === 'Contact' ? 'navTrue' : 'navFalse'}
                     >Contact
                 </a>
             </button>
-            <button>
+
+            <button className="navButton">
                 <a
                     href="#Resume"
-                    // onClick={() => handlePageChange('Resume')}
-                    // className={currentPage === 'Resume' ? 'nav-linkActive' : 'nav-link'}
+                    onClick={() => handlePageChange('Resume')}
+                    className={currentPage === 'Resume' ? 'navTrue' : 'navFalse'}
                     >Resume
                 </a>
             </button>
+
         </div>
     )
 }
