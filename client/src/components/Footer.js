cc01ebc78686bd399b5fdb9c778b9df19206df7b
azariahgarrido <azariahdanielgarrido@gmail.com>
2022-09-27 14:36:12 -0700

    Added desktop link

--- client/src/components/Footer.js
+++ client/src/components/Footer.js
@@ -1,9 +1,13 @@
 export default function Footer() {
+
     return(
         <footer>
-            <div><a href="https://github.com/azariahgarrido" target="_blank" rel="noreferrer"><img className="toe" src='https://www.pine64.org/wp-content/uploads/2019/04/github-logo.jpg' alt="FailedToLoadImg"></img></a></div>
-            <div><a href="https://www.linkedin.com/in/azariah-garrido-241792235/" target="_blank" rel="noreferrer"><img className="toe" src="https://th.bing.com/th/id/OIP.bBQANbh1YB53wIhXO2313gAAAA?pid=ImgDet&rs=1" alt="FailedToLoadImg"></img></a></div>
-            <div><a href="https://www.facebook.com/azariah.garrido/" target="_blank" rel="noreferrer"><img className="toe" src="https://th.bing.com/th/id/OIP.xHe4-6tTi8FeIRYlG43PAgAAAA?pid=ImgDet&rs=1" alt="FailedToLoadImg"></img></a></div>
+            <div className="flatImg">
+                <div><a href="https://github.com/azariahgarrido" target="_blank" rel="noreferrer"><img className="toe" src='https://www.pine64.org/wp-content/uploads/2019/04/github-logo.jpg' alt="FailedToLoadImg"></img></a></div>
+                <div><a href="https://www.linkedin.com/in/azariah-garrido-241792235/" target="_blank" rel="noreferrer"><img className="toe" src="https://th.bing.com/th/id/OIP.bBQANbh1YB53wIhXO2313gAAAA?pid=ImgDet&rs=1" alt="FailedToLoadImg"></img></a></div>
+                <div><a href="https://www.facebook.com/azariah.garrido/" target="_blank" rel="noreferrer"><img className="toe" src="https://th.bing.com/th/id/OIP.xHe4-6tTi8FeIRYlG43PAgAAAA?pid=ImgDet&rs=1" alt="FailedToLoadImg"></img></a></div>
+            </div>
+            <button className="mobileB"><a href="https://azariahgarrido.github.io/azariah-garrido-resume/" target="_blank" rel="noreferrer">Switch to Desktop</a></button>
         </footer>
     )
 }
